61bbb2c928f3d468241b2e5ba3b0fc283928625f
Daniel Sokolov <d.sokolov@roseltorg.ru>
2021-10-07 13:07:46 +0300

    Поправил дату и отображение сообщений

--- app/src/Services/Message/MessageService.php
+++ app/src/Services/Message/MessageService.php
@@ -8,6 +8,7 @@ use App\Entity\Message;
 use App\Entity\Participant;
 use App\Repository\MessageRepository;
 use DateTime;
+use DateTimeImmutable;
 
 class MessageService
 {
@@ -37,7 +38,7 @@ class MessageService
     {
         $message = new Message();
         $message->setContent($content);
-        $message->setCreatedAt(new DateTime('NOW'));
+        $message->setCreatedAt(new DateTimeImmutable());
         $message->setIsShow(1);
         $message->setParticipant($participant);
         $this->messageRepository->setCreate($message);
